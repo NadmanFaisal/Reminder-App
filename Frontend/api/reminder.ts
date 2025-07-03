36c2f31d9ec13e672ee3d28abf503efa7edf5461
Nadman Faisal <nadman@chalmers.se>
2025-07-03 21:23:32 +0600

    #52 Add comments to reminder API calls

--- Frontend/api/reminder.ts
+++ Frontend/api/reminder.ts
@@ -1,5 +1,20 @@
 import api from "./api"
 
+/**
+ * Create reminder using HTTP requests to the backend.
+ * @param title Title of the reminder
+ * @param description Description of the reminder
+ * @param userEmail User's email for whom the reminder is to be created
+ * @param completed Boolean indicating whether the reminder will remain 
+ * as completed/incompleted in the backend
+ * @param deleted Boolean indicating whether the reminder will remain 
+ * as deleted or not in the backend
+ * @param createdAt Time of creation of the reminder
+ * @param lastModified Latest date when the reminder was modified
+ * @param remindAt Date and time when the reminder is to be triggered
+ * @param token Token for authenticating the user
+ * @returns Response object from the backend
+ */
 export async function createReminder(title: string, description: string, userEmail: string, completed: boolean, deleted: boolean, createdAt: Date, lastModified: Date, remindAt: Date, token: string) {
   try {
     const response = await api.post('/ReminderService/CreateReminder', {
@@ -16,6 +31,12 @@ export async function createReminder(title: string, description: string, userEma
   }
 }
 
+/**
+ * Fetch the user's reminders from the backend
+ * @param email Email of the specific user to get the specific reminders
+ * @param token Token for authenticating the user
+ * @returns Response object from the backend
+ */
 export async function getUserReminders(email: string, token: string) {
   try {
     const response = await api.get('/ReminderService/GetUserReminders', {
@@ -35,6 +56,12 @@ export async function getUserReminders(email: string, token: string) {
   }
 }
 
+/**
+ * Fetches a particular reminder from the backend
+ * @param reminderId The particular reminder's unique ID
+ * @param token Token for authenticating the user
+ * @returns Response object from the backend
+ */
 export async function getReminder(reminderId: string, token: string) {
   try {
     const response = await api.get('/ReminderService/GetReminder', {
@@ -54,6 +81,12 @@ export async function getReminder(reminderId: string, token: string) {
   }
 }
 
+/**
+ * Patch the reminder's completed status to true/false
+ * @param reminderId The particular reminder's unique ID
+ * @param token Token for authenticating the user
+ * @returns Response object from the backend
+ */
 export async function updateReminderCompleteStatus(reminderId: string, token: string) {
   try {
     const response = await api.patch('/ReminderService/UpdateCompleteStatus', {
@@ -71,6 +104,15 @@ export async function updateReminderCompleteStatus(reminderId: string, token: st
   }
 }
 
+/**
+ * Update the fields of the reminder in the backend
+ * @param reminderId Unique ID of the reminder to be updated
+ * @param title Title of the reminder
+ * @param description Description of the reminder
+ * @param remindAt Date and time for triggering the reminder
+ * @param token Token for authorizing the user
+ * @returns Response object from the backend
+ */
 export async function updateReminder(reminderId: string, title: string, description: string, remindAt: Date, token: string) {
   try {
     const response = await api.patch('ReminderService/UpdateReminder', {
@@ -88,6 +130,12 @@ export async function updateReminder(reminderId: string, title: string, descript
   }
 }
 
+/**
+ * Delete the reminder from the backend
+ * @param reminderId The particular reminder's unique ID
+ * @param token Token for authorizing the user
+ * @returns Response object from the backend
+ */
 export async function deleteReminder(reminderId: string, token: string) {
   try {
     const response = await api.patch('/ReminderService/ChangeReminderDeleteStatus', {
